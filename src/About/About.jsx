59332cc4db725e6ececbe357505b5f64523e6f60
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-11 23:49:55 -0400

    Add About and routing

---
+++ src/About/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+    return (
+        <p>
+            Welcome to Floodlight Pedals! The reason Floodlight exists is to glorify God and have fun making pedals.
+            <br/><br/>
+            I make pedals based on other circuits. They are all named for attributes of those who follow Jesus.
+            If you follow Him, those things are true of you! May this site and these pedals remind you of that.
+            If you don't follow Jesus, may this site and these pedals show you how good He is.
+        </p>
+    );
+}
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import About from './About/About';
 import Header from './Header/Header';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <Header />
-        
+        <div>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path='/' element={<About />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
