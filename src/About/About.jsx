75bb5f7a7538e7391b0bdc13f2124523c0495d9c
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-12 17:20:42 -0400

    Add contact info Updats About CSS

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,6 +8,11 @@ function About() {
             I make pedals based on other circuits. They are all named for attributes of those who follow Jesus.
             If you follow Him, those things are true of you! May this site and these pedals remind you of that.
             If you don't follow Jesus, may this site and these pedals show you how good He is.
+            <br/><br/>
+            If you have questions, comments, or requests, I'd love to talk with you!
+            <br/><br/>
+            Feel free to email me
+            at <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>.
         </p>
     );
 }
