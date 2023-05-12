c7d2a10c44204bbc7d32a199293acaa889cdd711
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-12 16:40:46 -0400

    Add Product component Clean up mods CSS

--- src/Mods/Mods.jsx
+++ src/Mods/Mods.jsx
@@ -1,22 +1,17 @@
 import './Mods.css';
+import Product from '../Product/Product';
 
 function Mods() {
     return (
-        <div>
+        <div class="mods">
             <p>
                 These are the mods I have done before and could do again.
                 If you're interested in one of these or want a mod not listed here, email me
                 at <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>. Price is negotiable.
             </p>
-            <div class="mod-container">
-                <div class="mod-cell">
-                    <img src="assets/img/mods/ds-1-tigress.jpg" alt="Boss DS-1 Tigress Mod"/>
-                    <p>Tigress Mod (Boss DS-1 shown here)</p>
-                </div>
-                <div class="mod-cell">
-                    <img src="assets/img/mods/ch-1-vibrato.jpg" alt="Boss CH-1 Vibrato Mod"/>
-                    <p>Boss CH-1 Vibrato Mod</p>
-                </div>
+            <div class="grid-container">
+                <Product />
+                <Product />
             </div>
         </div>
     );
---
+++ src/Product/Product.jsx
@@ -0,0 +1,12 @@
+import './Product.css';
+
+function Product() {
+    return (
+        <div class="product">
+            <img src="assets/img/mods/ds-1-tigress.jpg" alt="Boss DS-1 Tigress Mod"/>
+            <p>Tigress Mod (Boss DS-1 shown here)</p>
+        </div>
+    );
+}
+
+export default Product;
