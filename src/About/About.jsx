5754fe8cb7f97ca8f6721a89c14a1377b31f5c3f
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-12 17:24:01 -0400

    Update About content

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -5,14 +5,10 @@ function About() {
         <p class="about">
             Welcome to Floodlight Pedals! The reason Floodlight exists is to glorify God and have fun making pedals.
             <br/><br/>
-            I make pedals based on other circuits. They are all named for attributes of those who follow Jesus.
-            If you follow Him, those things are true of you! May this site and these pedals remind you of that.
-            If you don't follow Jesus, may this site and these pedals show you how good He is.
+            I make pedals based on other circuits and perform mods to existing pedals.
+            If you are interested in anything listed here or want something custom, please reach out!
             <br/><br/>
-            If you have questions, comments, or requests, I'd love to talk with you!
-            <br/><br/>
-            Feel free to email me
-            at <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>.
+            My email is <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>.
         </p>
     );
 }
