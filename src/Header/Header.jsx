3adc7956e91e202923e3a870a50013144e9ff658
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-13 08:11:20 -0400

    Move and fix image in header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,14 @@
 import './Header.css';
+import logoImg from '../assets/img/logo-symbol.svg';
+import logoText from '../assets/img/logo-text.svg';
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header>
             <Link to="/" className="logo">
-                <img id="logo-img" src="./assets/img/logo-symbol.svg" alt="Floodlight Pedals Logo"/>
-                <img id="logo-text" src="./assets/img/logo-text.svg" alt="Floodlight Pedals Logo Text"/>
+                <img id="logo-img" src={logoImg} alt="Floodlight Pedals Logo"/>
+                <img id="logo-text" src={logoText} alt="Floodlight Pedals Logo Text"/>
             </Link>
             <i id="menu-btn" className="fa-solid fa-bars"></i>
             <div className="links">
