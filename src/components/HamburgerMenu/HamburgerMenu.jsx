9132ed959cee9562c15e1afe672bfd979cfc1f9f
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-13 23:34:14 -0400

    Menu stopping poingt

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,6 +1,15 @@
 import './HamburgerMenu.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+function openMenu() {
+    document.getElementById('side-menu-wrapper').classList.add('open');
+}
+
+function closeMenu() {
+    document.getElementById('side-menu-wrapper').classList.remove('open');
+}
 
 function HamburgerMenu() {
     return (
@@ -9,7 +18,21 @@ function HamburgerMenu() {
                 icon={faBars}
                 size='3x'
                 id='menu-btn'
+                onClick={openMenu}
             />
+            <div id='side-menu-wrapper' onClick={closeMenu}>
+                <div id='side-menu' onClick={event => event.stopPropagation()}>
+                    <FontAwesomeIcon
+                        id='close-menu-btn'
+                        icon={faX}
+                        size='xl'
+                        onClick={closeMenu}
+                    />
+                    <Link to="/">ABOUT</Link>
+                    <Link to="/pedals">PEDALS</Link>
+                    <Link to="/mods">MODS</Link>
+                </div>
+            </div>
         </>
     );
 }
