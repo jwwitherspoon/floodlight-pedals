122ac80e70d655123fbc979bf7154748dc5ad252
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-05-17 21:05:23 -0400

    Add coming soon section to pedals

--- src/components/Pedals/Pedals.jsx
+++ src/components/Pedals/Pedals.jsx
@@ -1,16 +1,23 @@
 import './Pedals.css';
+import logoImg from '../../assets/img/logo-symbol.svg';
 
 function Pedals() {
     return (
-        <div className="pedals">
+        <div className='coming-soon'>
+            <img id='coming-soon-logo' src={logoImg} alt="Floodlight Pedals Logo"/>
             <p>
-                These are my own takes on existing circuits.
-                If you're interested in one of these, email me
-                at <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>.
+                Floodlight brand pedals coming soon!
             </p>
-            <div className="grid-container">
-            </div>
         </div>
+        // <div className="pedals">
+        //     <p>
+        //         These are my own takes on existing circuits.
+        //         If you're interested in one of these, email me
+        //         at <a href="mailto:floodlight.pedals@gmail.com">floodlight.pedals@gmail.com</a>.
+        //     </p>
+        //     <div className="grid-container">
+        //     </div>
+        // </div>
     );
 }
 
